cd047f226e6dd76d5b3439f77d1ae188eba61e84
ixio0330 <ixio0330@gmail.com>
2026-03-10 10:01:48 +0900

    style: use custom color

--- src/context/ToastContext.tsx
+++ src/context/ToastContext.tsx
@@ -79,10 +79,10 @@ export function ToastProvider({ children }: { children: ReactNode }) {
                 absolute left-0 top-0 bottom-0 w-1.5
                 ${
                   toast.type === "success"
-                    ? "bg-emerald-500"
+                    ? "bg-success"
                     : toast.type === "error"
-                      ? "bg-red-500"
-                      : "bg-zinc-500"
+                      ? "bg-danger"
+                      : "bg-zinc-600"
                 }
               `}
             />
