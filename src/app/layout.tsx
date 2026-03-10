3762d9aa472afc15a288cb05b148a35251dd3b43
ixio0330 <ixio0330@gmail.com>
2026-03-10 12:00:50 +0900

    chore: ios PWA 스플래시 대응

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,15 @@
 import { MockDataProvider } from "@/context/MockDataContext";
 import { ToastProvider } from "@/context/ToastContext";
+import { Viewport } from "next";
 import "./globals.css";
+
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
+  themeColor: "#ffffff",
+};
 
 export default function RootLayout({
   children,
@@ -12,12 +21,9 @@ export default function RootLayout({
       <head>
         <title>WIG Tracker</title>
         <meta name="description" content="가장 중요한 목표에 집중하세요." />
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
-        />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="WIG Tracker" />
         <meta name="theme-color" content="#EFF0FA" />
         <link rel="manifest" href="/manifest.json" />
         <link rel="icon" href="/favicon.svg" />
@@ -46,28 +52,13 @@ export default function RootLayout({
         />
         <link
           rel="apple-touch-startup-image"
-          href="/splash/apple-splash-1179-2556.jpg"
-          media="(device-width: 393px) and (device-height: 852px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
+          href="/splash/apple-splash-1640-2360.jpg"
+          media="(device-width: 820px) and (device-height: 1180px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
         />
         <link
           rel="apple-touch-startup-image"
-          href="/splash/apple-splash-1284-2778.jpg"
-          media="(device-width: 428px) and (device-height: 926px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
-        />
-        <link
-          rel="apple-touch-startup-image"
-          href="/splash/apple-splash-1170-2532.jpg"
-          media="(device-width: 390px) and (device-height: 844px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
-        />
-        <link
-          rel="apple-touch-startup-image"
-          href="/splash/apple-splash-1242-2688.jpg"
-          media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
-        />
-        <link
-          rel="apple-touch-startup-image"
-          href="/splash/apple-splash-1125-2436.jpg"
-          media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
+          href="/splash/apple-splash-1260-2736.jpg"
+          media="(device-width: 420px) and (device-height: 912px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
         />
 
         <link
