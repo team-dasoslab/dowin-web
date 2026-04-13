f27ffa0143d7e6a361a222bb9b88405c84ad907a
ixio0330 <ixio0330@gmail.com>
2026-04-14 01:28:41 +0900

    feat: 제품 업데이트 태그 코치마크 추가

--- src/app/(protected)/setup/_components/SetupCoachmark.tsx
+++ src/app/(protected)/setup/_components/SetupCoachmark.tsx
@@ -1,30 +1,41 @@
 "use client";
 
 import {
+  SETUP_COACHMARK_LEAD_TAGS_QUERY,
   SETUP_COACHMARK_OPTIONS,
   SETUP_COACHMARK_STEPS,
   SETUP_COACHMARK_STYLES,
+  SETUP_LEAD_TAGS_COACHMARK_STEPS,
   handleSetupCoachmarkEvent,
 } from "@/app/(protected)/setup/_lib/setup-coachmark";
 import { Joyride, type EventData } from "react-joyride";
 
 interface SetupCoachmarkProps {
+  mode?: "default" | typeof SETUP_COACHMARK_LEAD_TAGS_QUERY;
   isRunning: boolean;
   setIsRunning: (value: boolean) => void;
 }
 
 export function SetupCoachmark({
   isRunning,
+  mode = "default",
   setIsRunning,
 }: SetupCoachmarkProps) {
   const handleEvent = (data: EventData) => {
-    handleSetupCoachmarkEvent(data, setIsRunning);
+    handleSetupCoachmarkEvent(data, setIsRunning, {
+      persistDismissed: mode === "default",
+    });
   };
+
+  const steps =
+    mode === SETUP_COACHMARK_LEAD_TAGS_QUERY
+      ? SETUP_LEAD_TAGS_COACHMARK_STEPS
+      : SETUP_COACHMARK_STEPS;
 
   return (
     <Joyride
       run={isRunning}
-      steps={SETUP_COACHMARK_STEPS}
+      steps={steps}
       onEvent={handleEvent}
       options={SETUP_COACHMARK_OPTIONS}
       styles={SETUP_COACHMARK_STYLES}
