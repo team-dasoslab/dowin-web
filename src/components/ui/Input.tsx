96efdd6648d3479f96f44a4b85a18f883309ca1d
ixio0330 <ixio0330@gmail.com>
2026-04-26 17:32:50 +0900

    style: iOS 입력창 자동 대문자 방지 및 폰트 크기 최적화

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -4,11 +4,13 @@ export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> 
   ref?: React.Ref<HTMLInputElement>;
 }
 
-export const Input = ({ className, ref, ...props }: InputProps) => {
+export const Input = ({ className, ref, autoCapitalize = "none", autoCorrect = "off", ...props }: InputProps) => {
   return (
     <input
       className={className}
       ref={ref}
+      autoCapitalize={autoCapitalize}
+      autoCorrect={autoCorrect}
       {...props}
     />
   );
