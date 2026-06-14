1a3aff70bbe37bd04e1524371d6661e1546107bb
ixio0330 <ixio0330@gmail.com>
2026-06-14 11:12:28 +0900

    chore: 스토어 링크 연결

---
+++ src/config/links.ts
@@ -0,0 +1,4 @@
+export const EXTERNAL_LINKS = {
+  APP_STORE: "https://apps.apple.com/kr/app/dowin/id6764197512",
+  PLAY_STORE: "https://play.google.com/store/apps/details?id=com.dasoslab.dowin",
+} as const;
